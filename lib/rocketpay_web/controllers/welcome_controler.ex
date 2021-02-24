127867e0ee1a5f147c1e3e9798c981b1474c6bcc
Francisco Castro <francisco.ruf@gmail.com>
2021-02-24 10:46:43 -0300

    finishing first class

--- lib/rocketpay_web/controllers/welcome_controler.ex
+++ lib/rocketpay_web/controllers/welcome_controler.ex
@@ -1,7 +1,23 @@
 defmodule RocketpayWeb.WelcomeController do
   use RocketpayWeb, :controller
 
-  def index(conn, _params) do
-    text(conn, "welcome to the rocketpay api")
+  alias Rocketpay.Numbers
+
+  def index(conn, %{"filename" => filename}) do
+    filename
+    |> Numbers.sum_from_file()
+    |> handle_response(conn)
+  end
+
+  defp handle_response({:ok, %{result: result}}, conn) do
+    conn
+    |> put_status(:ok)
+    |> json(%{message: "welcome to rocketpay API, here is your number #{result}"})
+  end
+
+  defp handle_response({:error, reason}, conn) do
+    conn
+    |> put_status(:bad_request)
+    |> json(reason)
   end
 end
