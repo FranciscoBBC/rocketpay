ab20bbd4bd631fa8104d5a81f41158bb06a92bda
Francisco Castro <francisco.ruf@gmail.com>
2021-02-26 08:35:31 -0300

    linking account and user

--- lib/rocketpay/users/create.ex
+++ lib/rocketpay/users/create.ex
@@ -1,9 +1,43 @@
 defmodule Rocketpay.Users.Create do
-  alias Rocketpay.{Repo, User}
+  alias Rocketpay.{Account, Repo, User}
+  alias Ecto.Multi
 
   def call(params) do
-    params
-    |> User.changeset()
-    |> Repo.insert()
+    Multi.new()
+    |> Multi.insert(:create_user, User.changeset(params))
+    |> Multi.run(:create_account, fn repo, %{create_user: user} ->
+      insert_account(repo, user)
+    end)
+    |> Multi.run(:preload_data, fn repo, %{create_user: user} ->
+      preload_data(repo, user)
+    end)
+    |> run_transaction()
+
+    # params
+    # |> User.changeset()
+    # |> Repo.insert()
+  end
+
+  defp insert_account(repo, user) do
+    user.id
+    |> account_changeset()
+    |> repo.insert()
+  end
+
+  defp account_changeset(user_id) do
+    params = %{user_id: user_id, balance: "0.00"}
+
+    Account.changeset(params)
+  end
+
+  defp preload_data(repo, user) do
+    {:ok, repo.preload(user, :account)}
+  end
+
+  defp run_transaction(multi) do
+    case Repo.transaction(multi) do
+      {:error, __operation, reason, __changes} -> {:error, reason}
+      {:ok, %{preload_data: user}} -> {:ok, user}
+    end
   end
 end
--- lib/rocketpay_web/views/user_view.ex
+++ lib/rocketpay_web/views/user_view.ex
@@ -1,13 +1,24 @@
 defmodule RocketpayWeb.UserView do
-  alias Rocketpay.User
+  alias Rocketpay.{User, Account}
 
-  def render("create.json", %{user: %User{id: id, name: name, nickname: nickname}}) do
+  def render("create.json", %{
+        user: %User{
+          id: id,
+          name: name,
+          nickname: nickname,
+          account: %Account{id: account_id, balance: balance}
+        }
+      }) do
     %{
       message: "user created",
       user: %{
         id: id,
         name: name,
-        nickname: nickname
+        nickname: nickname,
+        account: %{
+          id: account_id,
+          balance: balance
+        }
       }
     }
   end
