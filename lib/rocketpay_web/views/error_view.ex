78b3d355c8de618892f015262f5f76481e2dac7c
Francisco Castro <francisco.ruf@gmail.com>
2021-02-25 21:25:40 -0300

    creating account schema

--- lib/rocketpay_web/views/error_view.ex
+++ lib/rocketpay_web/views/error_view.ex
@@ -1,16 +1,23 @@
 defmodule RocketpayWeb.ErrorView do
   use RocketpayWeb, :view
 
-  # If you want to customize a particular status code
-  # for a certain format, you may uncomment below.
-  # def render("500.json", _assigns) do
-  #   %{errors: %{detail: "Internal Server Error"}}
-  # end
+  import Ecto.Changeset, only: [traverse_errors: 2]
 
-  # By default, Phoenix returns the status message from
-  # the template name. For example, "404.json" becomes
-  # "Not Found".
+  alias Ecto.Changeset
+
   def template_not_found(template, _assigns) do
     %{errors: %{detail: Phoenix.Controller.status_message_from_template(template)}}
+  end
+
+  def render("400.json", %{result: %Changeset{} = changeset}) do
+    %{mesage: translate_errors(changeset)}
+  end
+
+  defp translate_errors(changeset) do
+    traverse_errors(changeset, fn {msg, opts} ->
+      Enum.reduce(opts, msg, fn {key, value}, acc ->
+        String.replace(acc, "%{#{key}}", to_string(value))
+      end)
+    end)
   end
 end
